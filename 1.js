aa0d3f08d4c74b67cbd1dc0db885f77af864e242
buynlt <thanhbuynitu@gmail.com>
2020-10-03 22:44:21 +0700

    update addClass Ex

--- 1.js
+++ 1.js
@@ -1,4 +1,6 @@
-
+function myFuncAddClass(){
+	$('#add_class').addClass("redClass");
+}
 function setCSS(){
 $('p:nth-child(2)').css("font-style", "italic");
 $('p:nth-child(3)').css("color", "yellow");
