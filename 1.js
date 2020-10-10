cf609fccab981ccfdb084c68d2199762e4417627
buynlt <thanhbuynitu@gmail.com>
2020-10-10 22:58:12 +0700

    kiem ta

--- 1.js
+++ 1.js
@@ -9,3 +9,11 @@ $('p:nth-child(3)').css("color", "yellow");
 function addToggleClass(){
 	$('#add_toggle_class').toggleClass("redClass");
 }
+
+function slideUp() {
+$("#divMsg").slideUp();
+}
+
+function slideDown() {
+$("#divMsgDown").slideDown();
+}
